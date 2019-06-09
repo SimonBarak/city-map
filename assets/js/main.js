b946480c925de15a1f9700e6cad3a52578656b23
Schittaci-air <barak.simon.as@gmail.com>
2019-06-09 10:19:07 +0200

    remove accesToken

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,9 +37,6 @@ const addMapListener = obj => {
   });
 };
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibG9uZXJzY29tcGFueSIsImEiOiJjandqNTlsYXIwNjduNDRwNjhkemhhZXVtIn0.c2PlH4OqBZUOQgDM4hg7fw";
-
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/lonerscompany/cjwjajpvb1ly61cnqacst6b3k",
